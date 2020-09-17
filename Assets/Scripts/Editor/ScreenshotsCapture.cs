100bab3b689847b25bbf0167fae346dc2106cb98
Abdulla Aldandarawy <abdullah@teli.hku.hk>
2020-09-17 18:39:48 +0800

    initialize repo with URP template with ShaderGraph and URP packages in Asset folder

---
+++ Assets/Scripts/Editor/ScreenshotsCapture.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ScreenshotsCapture 
+{
+    static string folderName = "Screenshots";
+
+    [MenuItem("Tools/CaptureSceenshot #S")]
+    static void AnchorsToCorners()
+    {
+        if (!System.IO.Directory.Exists(folderName))
+            System.IO.Directory.CreateDirectory(folderName);
+        ScreenCapture.CaptureScreenshot(folderName+"/"+System.DateTime.Now.ToString("ddMMyyHHmmss") + ".png");
+    }
+}
